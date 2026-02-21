2dabcc7114237b82f41651897fbe900945029ceb
jincheol5 <oscar4293@gmail.com>
2026-02-22 00:48:59 +0900

    update

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-import cv2
-import easyocr
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+from PIL import Image
 
+model_name = "team-lucid/trocr-small-korean"
+processor = TrOCRProcessor.from_pretrained(model_name)
+model = VisionEncoderDecoderModel.from_pretrained(model_name)
 
+image = Image.open("food1.png").convert("RGB")
+pixel_values = processor(image, return_tensors="pt").pixel_values
 
-# ----------------------------
-# 3. EasyOCR 적용
-# ----------------------------
-
-reader = easyocr.Reader(['ko','en'], gpu=False)
-
-results = reader.readtext("processed.png")
-
-# ----------------------------
-# 4. 텍스트만 출력
-# ----------------------------
-
-for bbox, text, confidence in results:
-    print(text)
+output_ids = model.generate(pixel_values)
+print(processor.decode(output_ids[0], skip_special_tokens=True))
